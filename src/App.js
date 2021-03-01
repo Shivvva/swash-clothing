673d62e92f641374dea055c0f9511b6486370d1a
shivaswa <psshivvvakumar@gmail.com>
2021-03-02 04:26:52 +0530

    menu item withRouter and opens hats page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Switch,Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () =>(
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+// switch used to match the path
+// route used to access pages by specific URl
+//exact checks exact match of path
 function App() {
   return (
     <div >
-     <HomePage/>
+      <Switch>
+      <Route exact path = '/' component= {HomePage}/>
+      <Route exact path = '/hats' component= {HatsPage}/>
+      </Switch>
     </div>
   );
 }
