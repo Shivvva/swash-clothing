3daff4948fb687e4ab2e9a8e00a031c302e9092d
shivaswa <psshivvvakumar@gmail.com>
2021-03-02 06:49:48 +0530

    adding header compnent

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
-
+import Header from './components/header/header.component.jsx';
 // switch used to match the path
 // route used to access pages by specific URl
 //exact checks exact match of path
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
       <Route exact path = '/' component= {HomePage}/>
       <Route exact path = '/shop' component= {ShopPage}/>
